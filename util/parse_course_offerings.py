ae1bbe555e9558071ba13315946df83e8cfb3b05
David Manouchehri <david@davidmanouchehri.com>
2015-02-22 16:24:22 -0330

    Parser is now able to print out every course and CRN.

--- util/parse_course_offerings.py
+++ util/parse_course_offerings.py
@@ -8,7 +8,7 @@ print('Memorial University course offerings parser, written by ' + __author__ + 
 directory = 'html/'
 
 fields = [5, 10, 38, 42, 48, 53, 67, 72, 77, 85, 91, 103, 108, 112, 117, 122, 127, 133, 137]
-global short_subject, course_code, crn
+
 for file in os.listdir(directory):
     print('Opening: ' + file)
     with open(directory + file) as f:
@@ -23,17 +23,16 @@ for file in os.listdir(directory):
                 pass
             elif "<" not in line and ">" not in line and len(line) in range(170, 180):
 
-                if line[fields[0]:fields[3]].strip():
-                    short_subject = line[0:fields[0]].strip()  # or short_subject
-                    course_code = line[fields[0]:fields[1]].strip()  # or course_code
+                if line[fields[0]:fields[2]].strip() \
+                        and (line[0:35].strip() and line[57:170].strip()):  # The Math Placement Test does weird things.
+                    short_subject = line[0:fields[0]].strip()
+                    course_code = line[fields[0]:fields[1]].strip()
                     course_short_description = line[fields[1]:fields[2]].strip()
-                    sec = line[fields[4]:fields[5]].strip()
-                    crn = line[fields[3]:fields[4]].strip()
+                    if line[fields[2]:fields[5]].strip():
+                        sec = line[fields[4]:fields[5]].strip()
+                        crn = line[fields[3]:fields[4]].strip()
                 else:
                     reg_details = line.strip()
 
-                print(short_subject + course_code + crn + ' from ' + file)
-
-                # print(crn + " " + sec)
-                '''if len(line) is 176:
-                    print(line)'''
+                print(short_subject + '-' + course_code + ' ' + crn + ' ' + course_short_description + ' ' + sec
+                      + ' from ' + file)
